fc796970da8e04e9be024303f9065969b26e0a9a
Habiba <habiba.elshaer004@gmail.com>
2026-07-02 21:05:29 +0300

    [base]

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import "./i18n";
+
+// original stylesheet cascade, unchanged, in the original order
+import "./CSS/normalize.css";
+import "./CSS/fontawesome.ts";
+import "./CSS/styles.css";
+import "./CSS/rtl.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
